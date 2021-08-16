5f2b99fafdf91467e81090db50d46dd5186c5771
yy78910 <80239432+yy78910@users.noreply.github.com>
2021-08-16 09:21:56 +0800

    Update yqq.js

--- yqq.js
+++ yqq.js
@@ -4,7 +4,21 @@ var pic = document.querySelector('img');
         document.addEventListener('mousemove', function(e) {
             var x = e.pageX;
             var y = e.pageY;
-            console.log('x坐标是' + x, 'y坐标是' + y);
             pic.style.left = x - 50 + 'px';
             pic.style.top = y - 40 + 'px';
  });
+function disableInfo() {
+    document.onkeydown = function() {
+        var e = window.event || arguments[0];
+        if(e.keyCode == 123) {
+            return false;
+        } else if((e.ctrlKey) && (e.shiftKey) && (e.keyCode == 73)) {
+            return false;
+        } else if((e.shiftKey) && (e.keyCode == 121)){
+            return false;
+        }
+    };
+ document.oncontextmenu = function() {
+        return false;
+    }
+}
